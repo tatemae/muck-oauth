d7a4068e4bc3e804b03007220ba3487073990a31
Justin Ball <justinball@gmail.com>
2009-12-21 11:45:39 -0700

    refactored oauth views

--- app/helpers/muck_oauth_helper.rb
+++ app/helpers/muck_oauth_helper.rb
@@ -1,23 +1,37 @@
 module MuckOauthHelper
   
+  # Renders fancybox javascript for oauth services
   def oauth_fancybox_scripts
     render :partial => 'oauth_consumers/oauth_fancybox_scripts'
   end
-  
-  def oauth_current_services
-    render :partial => 'oauth_consumers/oauth_current_services', :locals => { :consumer_tokens => oauth_consumer_tokens }
+
+  # Render a list of a user's current oauth services.
+  # user:           User for which to render services
+  # include_icons:  If true this will render an iconf for the service  
+  def oauth_current_services(user, include_icons = true)
+    render :partial => 'oauth_consumers/oauth_current_services', :locals => { :consumer_tokens => oauth_consumer_tokens(user), :include_icons => include_icons }
   end
   
-  def oauth_available_services
-    render :partial => 'oauth_consumers/oauth_available_services', :locals => { :services => oauth_services }
+  # Render a list of all available oauth services.
+  # include_icons: If true this will render an iconf for the service
+  def oauth_available_services(user = nil, include_icons = true)
+    render :partial => 'oauth_consumers/oauth_available_services', :locals => { :services => oauth_services(user), :include_icons => include_icons }
   end
   
-  def oauth_consumer_tokens
-    @oauth_consumer_tokens ||= ConsumerToken.all(:conditions => { :user_id => current_user.id })
+  # Gets an caches the given user's oauth tokens
+  def oauth_consumer_tokens(user)
+    if user.blank?
+      @all_oauth_consumer_tokens ||= ConsumerToken.all
+    else
+      @oauth_consumer_tokens ||= ConsumerToken.all(:conditions => { :user_id => user.id })
+    end
   end
   
-  def oauth_services
-    @oauth_services ||= OAUTH_CREDENTIALS.keys-oauth_consumer_tokens.collect{ |c| c.class.service_name }
+  # Gets and caches all services.  
+  # user:     If user is specified then the given user's current oauth services will be excluded from 
+  #           the returned list of services.
+  def oauth_services(user = nil)
+    @oauth_services ||= OAUTH_CREDENTIALS.keys-oauth_consumer_tokens(user).collect{ |c| c.class.service_name }
   end
   
 end
--- lib/muck_oauth/tasks.rb
+++ lib/muck_oauth/tasks.rb
@@ -18,7 +18,9 @@ module MuckOauth
             path = File.join(File.dirname(__FILE__), *%w[.. ..])
             system "rsync -ruv #{path}/db ."
             system "rsync -ruv #{path}/public ."
-            system "rsync -ruv #{path}/config/initializers ./config"
+            if !File.exists('./config/oauth_consumers.rb')
+              system "rsync -ruv #{path}/config/initializers/oauth_consumers.rb ./config/oauth_consumers.rb"
+            end
           end
         end
       end
