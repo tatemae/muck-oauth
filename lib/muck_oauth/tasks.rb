ed156a0da394498005652c64695c93220f47ee65
Justin Ball <justinball@gmail.com>
2010-02-17 17:15:57 -0700

    updated translations.  refactored rake task

--- lib/muck_oauth/tasks.rb
+++ lib/muck_oauth/tasks.rb
@@ -12,9 +12,9 @@ module MuckOauth
     def define
       
       namespace :muck do
-        namespace :oauth do
+        namespace :sync do
           desc "Sync required files from muck oauth."
-          task :sync do
+          task :oauth do
             path = File.join(File.dirname(__FILE__), *%w[.. ..])
             system "rsync -ruv #{path}/db ."
             #system "rsync -ruv #{path}/public ."
